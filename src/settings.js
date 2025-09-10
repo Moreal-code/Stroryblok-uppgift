7d826f77d23a4ad54beb2e249f02a1761d0746fb
Moreal-code <mohamed.dukale@yh.nackademin.se>
2025-09-10 15:58:31 +0200

    rättade url

--- src/settings.js
+++ src/settings.js
@@ -1,7 +1,6 @@
 // src/settings.js
 const SETTINGS = {
-  CMS_PUBLIC_KEY: process.env.STORYBLOK_PREVIEW_API_TOKEN,
-  SITE_URL: "stroryblok-uppgift.vercel.app",
+  CMS_PUBLIC_KEY: process.env.STORYBLOK_DELIVERY_API_ACCESS_TOKEN,
+  SITE_URL: "https://storyblok-uppgift.vercel.app",
 };
-
 export default SETTINGS;

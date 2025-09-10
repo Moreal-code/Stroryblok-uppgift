c9f79751f4e51a3143b152fdfc9ff0f12c2f8d35
Moreal-code <mohamed.dukale@yh.nackademin.se>
2025-09-10 15:46:44 +0200

    lägger till url i settings.js

--- src/settings.js
+++ src/settings.js
@@ -1,7 +1,7 @@
 // src/settings.js
 const SETTINGS = {
   CMS_PUBLIC_KEY: process.env.STORYBLOK_PREVIEW_API_TOKEN,
-  SITE_URL: "https://dindomän.vercel.app",
+  SITE_URL: "stroryblok-uppgift.vercel.app",
 };
 
 export default SETTINGS;

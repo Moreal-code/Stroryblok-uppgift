1fa85fe5bd22817257c9a5a4839662a18ec7af88
Moreal-code <mohamed.dukale@yh.nackademin.se>
2025-09-11 11:33:41 +0200

    rättar url i robots.js

--- src/app/robots.js
+++ src/app/robots.js
@@ -7,6 +7,6 @@ export default function robots() {
       allow: "/*",
       disallow: "/api/",
     },
-    sitemap: `https://${SETTINGS.SITE_URL}/sitemap.xml`,
+    sitemap: `${SETTINGS.SITE_URL}/sitemap.xml`,
   };
 }
--- src/settings.js
+++ src/settings.js
@@ -3,4 +3,5 @@ const SETTINGS = {
   CMS_PUBLIC_KEY: process.env.STORYBLOK_DELIVERY_API_ACCESS_TOKEN,
   SITE_URL: "https://storyblok-uppgift.vercel.app",
 };
+
 export default SETTINGS;

92eff4829b0c7d634a539ca44a106aceb55a32fe
Moreal-code <mohamed.dukale@yh.nackademin.se>
2025-09-11 13:57:14 +0200

    ändringar med jonatan

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -1,19 +1,26 @@
 import { getStoryblokApi } from "@/lib/storyblok";
-import { StoryblokStory } from '@storyblok/react/rsc';
+import { StoryblokStory } from "@storyblok/react/rsc";
+import { notFound } from "next/navigation";
 
 //Example of a static page
 export default async function Home() {
-  const { data } = await fetchData();
-  return (
-    <div className="page">
-      <StoryblokStory story={data.story} />
-    </div>
-  );
+  try {
+    const story = await fetchData();
+
+    return (
+      <div className="page">
+        <StoryblokStory story={story} />
+      </div>
+    );
+  } catch (error) {
+    console.warn(error);
+    return notFound();
+  }
 }
 
 export async function fetchData() {
   const storyblokApi = getStoryblokApi();
   return await storyblokApi.get("cdn/stories/home", {
-    version: "draft",
+    version: process.env.NODE_ENV === "production" ? "published" : "draft",
   });
 }
--- src/components/sb/Page.js
+++ src/components/sb/Page.js
@@ -5,9 +5,9 @@ import ServerComponent from "./ServerComponent";
 export default function Page({ blok }) {
   return (
     <main {...storyblokEditable(blok)}>
-      {blok.body.map((nestedBlok) => (
+      {/* {blok.body.map((nestedBlok) => (
         <ServerComponent blok={nestedBlok} key={nestedBlok._uid} />
-      ))}
+      ))} */}
     </main>
   );
 }
